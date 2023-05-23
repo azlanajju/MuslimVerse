a0964e92b7f2d4592acd31cc18412162f35026b7
azlanajju <muhammedazlan11@gmail.com>
2023-05-23 07:24:45 +0530

    added manqoos maulid

--- maulid/maulid.js
+++ maulid/maulid.js
@@ -1,8 +1,46 @@
-  const searchBar = document.getElementById("search-bar");
-  const placeholders = ["Search 'manqoos maulid'", "Search 'badr maulid'"];
-  let i = 0;
+const maulids = [
+  { name: '1. منقوص المولد', link: './manqoosMaulid/index.html', tags: ['manqoos', 'mankus', 'mankoos'] },
+  { name: '2. مولد  بدر', link: './maulid/badr.html', tags: ['badar', 'badr'] },
+];
 
-  setInterval(() => {
-    searchBar.setAttribute("placeholder", placeholders[i]);
-    i = (i + 1) % placeholders.length;
-  }, 2000);
+function filterMaulids(searchText) {بدر
+  return maulids.filter(maulid => {
+    const lowerCaseSearchText = searchText.toLowerCase();
+    const lowerCaseMaulidName = maulid.name.toLowerCase();
+    const tags = maulid.tags || [];
+
+    // Check if the search text is a partial match for the maulid name or any of its tags
+    return lowerCaseMaulidName.includes(lowerCaseSearchText) || tags.some(tag => tag.includes(lowerCaseSearchText));
+  });
+}
+
+function renderMaulids(maulids) {
+  const container = document.getElementById('container');
+  container.innerHTML = '';
+  for (const maulid of maulids) {
+    const maulidElement = document.createElement('div');
+    maulidElement.className = 'maulid';
+    maulidElement.innerHTML = maulid.name;
+    maulidElement.onclick = function () {
+      window.location = maulid.link;
+    };
+    container.appendChild(maulidElement);
+  }
+}
+
+const searchBar = document.getElementById('search-bar');
+const placeholders = ["Search 'manqoos maulid'", "Search 'badr maulid'"];
+let i = 0;
+
+setInterval(() => {
+  searchBar.setAttribute('placeholder', placeholders[i]);
+  i = (i + 1) % placeholders.length;
+}, 2000);
+
+searchBar.addEventListener('input', function (event) {
+  const searchText = event.target.value.trim();
+  const filteredMaulids = filterMaulids(searchText);
+  renderMaulids(filteredMaulids);
+});
+
+renderMaulids(maulids);
